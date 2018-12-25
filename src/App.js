f6c0860543509a0398a499d5097980cbe64f3325
Brent Newhall <brent@brentnewhall.com>
2018-12-25 15:58:39 -0500

    Added enemy ships.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,18 +6,47 @@ class App extends Component {
     super( props );
     this.state = {
       x: 100,
-      y: 100
-    }
+      y: 300
+    };
+    this.shipTypes = [
+      { speed: 0 },
+      { speed: 0.2 }
+    ];
+    this.ships = [
+      { id: 0, x: 50, y: 50, type: 1 },
+      { id: 1, x: 100, y: 50, type: 1 },
+      { id: 2, x: 150, y: 50, type: 1 }
+    ];
+    this.gameLoop = this.gameLoop.bind(this);
+    setInterval( this.gameLoop, 16 ); // 16 == 60fps
   }
+
+  // Update ship positions
+  gameLoop() {
+    this.ships.forEach( (ship) => {
+      ship.y += this.shipTypes[ship.type].speed;
+      if( ship.y > 500 ) // Wrap to top of screen
+        ship.y = 0;
+    })
+    this.forceUpdate();
+  }
+
   render() {
-    let ships = '';
+    let shipObjects = this.ships.map( (ship) => {
+      const enemyShipStyle = {
+        left: ship.x,
+        top: ship.y
+      }
+      return <img src={'/images/enemyRed' + ship.type + '.png'} alt='Enemy' style={enemyShipStyle} className='ship' key={'enemy'+ship.id} />
+    });
     let playerShipStyle = {
       left: this.state.x,
       top: this.state.y
     }
     return (
       <div className="App">
-        <img src='/images/playerShip1_blue.png' style={playerShipStyle} className='playerShip' alt='Player' />
+        <img src='/images/playerShip1_blue.png' style={playerShipStyle} className='ship' alt='Player' />
+        {shipObjects}
       </div>
     );
   }
