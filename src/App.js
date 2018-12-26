d2ee949a2fadd69ca9b96302e39087be31cf8f8e
Brent Newhall <brent@brentnewhall.com>
2018-12-25 19:31:58 -0500

    Added starfield background and lasers changing color of enemies upon laser hit

--- src/App.js
+++ src/App.js
@@ -15,9 +15,9 @@ class App extends Component {
       { speed: 0.2 }
     ];
     this.ships = [
-      { id: 0, x: 50, y: 50, type: 1, color: 'Red' },
-      { id: 1, x: 100, y: 50, type: 1, color: 'Red' },
-      { id: 2, x: 150, y: 50, type: 1, color: 'Red' }
+      { id: 0, x: 50, y: 50, type: 1, color: 0 },
+      { id: 1, x: 100, y: 50, type: 1, color: 0 },
+      { id: 2, x: 150, y: 50, type: 1, color: 0 }
     ];
     this.lasers = [];
     this.gameLoop = this.gameLoop.bind(this);
@@ -39,13 +39,24 @@ class App extends Component {
         ship.y = 0;
       }
     })
+    // Move lasers and update ships hit
     this.lasers.forEach( (laser, index) => {
       laser.y -= 5;
+      this.ships.forEach( (ship) => {
+        if( ship.x <= laser.x  &&  ship.x + 32 >= laser.x + 4  &&
+            ship.y + 32 >= laser.y - 2  &&  ship.y + 32 <= laser.y + 2 ) {
+          ship.color++; // Change ship color
+          if( ship.color > 2 )  ship.color = 0;
+        }
+      });
       if( laser.y <= 0 ) { // Remove
         this.lasers.splice( index, 1 );
       }
     })
-    if( this.state.shipMovement !== 0 ) {
+    if( this.state.shipMovement < 0  &&  this.state.x > 0 ) {
+      this.setState( { x: this.state.x + this.state.shipMovement } )
+    }
+    else if( this.state.shipMovement > 0  &&  this.state.x < 600 ) {
       this.setState( { x: this.state.x + this.state.shipMovement } )
     }
     this.forceUpdate();
@@ -80,14 +91,17 @@ class App extends Component {
         left: ship.x,
         top: ship.y
       }
-      return <img src={'/images/enemy' + ship.color + ship.type + '.png'} alt='Enemy' style={enemyShipStyle} className='ship' key={'enemy'+ship.id} />
+      return <img src={'/images/enemy' + this.shipColors[ship.color] +
+          ship.type + '.png'} alt='Enemy' style={enemyShipStyle}
+          className='ship' key={'enemy'+ship.id} />
     });
     let laserObjects = this.lasers.map( (laser, index) => {
       const laserStyle = {
         left: laser.x,
         top: laser.y
       }
-      return <img src='/images/laserRed01.png' alt='Laser' style={laserStyle} className='laser' key={'laser' + index} />
+      return <img src='/images/laserRed01.png' alt='Laser' style={laserStyle}
+          className='laser' key={'laser' + index} />
     })
     let playerShipStyle = {
       left: this.state.x,
@@ -96,7 +110,8 @@ class App extends Component {
     return (
       <div className="App" onKeyDown={this.keyDown} onKeyUp={this.keyUp}>
         {laserObjects}
-        <img src='/images/playerShip1_blue.png' style={playerShipStyle} className='ship' alt='Player' />
+        <img src='/images/playerShip1_blue.png' style={playerShipStyle}
+            className='ship' alt='Player' />
         {shipObjects}
       </div>
     );
